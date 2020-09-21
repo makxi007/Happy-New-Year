19f36aada089a32b9a589980d6e744c172c1637e
Maks <Mtolstov_2000@mail.ru>
2020-09-21 16:58:21 +0600

    4 commit

--- JavaScript_test/script.js
+++ JavaScript_test/script.js
@@ -1,8 +1,12 @@
 var day_list = document.getElementsByClassName("day_list");
+var day = document.getElementById("day");
+
+var weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
 for (var i = 0; i < day_list.length; i++){
 	day_list[i].onmouseover = day_list[i].onmouseout = handler;
 	day_list[i].innerHTML = "<p>Day" + (i+1) + "</p>";
+	day_list[i].addEventListener("click", function(){showDay();});
 }
 
 function handler(event){
@@ -12,4 +16,16 @@ function handler(event){
 	if (event.type == "mouseout"){
 		event.target.style.color = "";
 	}
+}
+
+function showDay(){
+	for(var i = 0; i < weekdays.length; i++){
+		day.innerHTML = "<p>" + weekdays[i] + "</p>";
+		day.style.display = "flex";
+	}
+	
+}
+
+function comparison(){
+	
 }
